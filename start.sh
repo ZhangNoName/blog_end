8be638482224f31fb319a10b1ff3ad33ec2915fc
tencent cloud <1623190186@qq.com>
2024-09-12 22:42:01 +0800

    feat:完善项目结构及启动

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export PYTHONPATH=$PYTHONPATH:$(pwd)/src
-poetry run uvicorn src.main:app --host 0.0.0.0 --port 8000 --reload --log-level debug
+poetry run uvicorn main:app --host 0.0.0.0 --port 8000 --reload --log-level debug
